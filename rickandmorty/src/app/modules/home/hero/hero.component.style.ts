2b92c52713d83449872c82ecbaf6d110e4afb8fa
Leonardo Palamim <leonardo.palamim@taqtile.com.br>
2021-03-19 00:28:16 -0300

    fix: corrected home background

--- rickandmorty/src/app/modules/home/hero/hero.component.style.ts
+++ rickandmorty/src/app/modules/home/hero/hero.component.style.ts
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import { Color } from "src/atomic/obj.constants/constants";
 import { Grid } from "src/atomic/obj.grid/grid";
+import Background from "src/assets/img/background-wallpaper.jpg";
 
 export const HeroStyled = styled.div`
   margin-top: 0px;
   height: 100vh;
-  background: ${Color.White} url(background-wallpaper.jpg) center center no-repeat;
+  background: ${Color.White} url(${Background}) center center no-repeat;
   background-size: cover;
 `
 
